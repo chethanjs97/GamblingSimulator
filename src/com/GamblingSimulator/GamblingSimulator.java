ce23bd5539e5588ba695f35f79f59707a3f52481
chethanjs97 <chethanjschethu@gmail.com>
2020-12-03 10:21:56 +0530

    Added : Checking Gambler Win or Loose

--- src/com/GamblingSimulator/GamblingSimulator.java
+++ src/com/GamblingSimulator/GamblingSimulator.java
@@ -1,13 +1,31 @@
 package com.GamblingSimulator;
 
+import java.util.Random;
+
 public class GamblingSimulator {
-	public static int stack = 100;
-	public static int bet = 1;
+	public static final int STACK = 100;
+	public static final int BET = 1;
+	public static int Win = 0;
+	public static int Loose = 0;
+
+	public void checkWinOrLoose() {
+		Random Number = new Random();
+		int randomNumber = Number.nextInt(2);
+		if (randomNumber == 0) {
+			Win = BET;
+			System.out.println("Gambler Is Winner " + Win);
+		} else {
+			Loose = BET;
+			System.out.println("Gamler is not winner " + Loose);
+		}
+	}
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome To Gambling Simulator");
-		System.out.println(stack + " " + bet);
+		GamblingSimulator gamblingSimulator = new GamblingSimulator();
+		gamblingSimulator.checkWinOrLoose();
+
 	}
 
 }
