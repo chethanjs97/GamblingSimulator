571f8bb1ceee3b7e8eb96e56d60b1a24c1d04959
chethanjs97 <chethanjschethu@gmail.com>
2020-12-03 09:59:56 +0530

    Added : Declaring two varibles

--- src/com/GamblingSimulator/GamblingSimulator.java
+++ src/com/GamblingSimulator/GamblingSimulator.java
@@ -1,11 +1,13 @@
 package com.GamblingSimulator;
 
 public class GamblingSimulator {
+	public static int stack = 100;
+	public static int bet = 1;
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome To Gambling Simulator");
-
+		System.out.println(stack + " " + bet);
 	}
 
 }
