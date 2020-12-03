959031064b45fe0c20e7f6a2f149bddc6782799e
chethanjs97 <chethanjschethu@gmail.com>
2020-12-03 11:49:55 +0530

    Added : Total Money win or loose

--- src/com/GamblingSimulator/GamblingSimulator.java
+++ src/com/GamblingSimulator/GamblingSimulator.java
@@ -9,9 +9,9 @@ public class GamblingSimulator {
 	public static int loose = 0;
 	static Random Number = new Random();
 	static int randomNumber = Number.nextInt(2);
+
 	public static void checkWinOrLoose() {
-		
-		
+
 		if (randomNumber == 0) {
 			win = BET;
 			System.out.println("Gambler Is Winner " + win);
@@ -20,11 +20,11 @@ public class GamblingSimulator {
 			System.out.println("Gamler is not winner " + loose);
 		}
 	}
-	public static void WinOrLoose50Percent() {
-		
+
+	public static int WinOrLoose50Percent() {
+
 		int newStack = STACK;
 		while (newStack > 50 && newStack < 150) {
-			
 
 			if (randomNumber == 0) {
 				newStack = newStack + BET;
@@ -33,19 +33,36 @@ public class GamblingSimulator {
 			} else {
 				newStack = newStack - BET;
 				loose = BET;
-				System.out.println("Gamler is not winner" + loose);
+				System.out.println("Gamler is not winner :" + loose);
 			}
 		}
 		System.out.println("Total Stack is : " + newStack);
+		return newStack;
+	}
+
+	public static void TotalMoney() {
+		int day = 20;
+		int totalMoney = 0;
+		for (int i = 0; i <= day; i++) {
+			int temp = WinOrLoose50Percent();
+			if (temp - STACK > 50) {
+				totalMoney = totalMoney + (temp - STACK);
+				System.out.println("Win day " + i + "Money Won is" + totalMoney);
+			} else {
+				totalMoney = totalMoney + (STACK - temp);
+				System.out.println("loose day" + i + "Money Loss  is " + totalMoney);
+			}
+		}
+
 	}
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome To Gambling Simulator");
-		
+
 		checkWinOrLoose();
 		WinOrLoose50Percent();
-
+		TotalMoney();
 	}
 
 }
